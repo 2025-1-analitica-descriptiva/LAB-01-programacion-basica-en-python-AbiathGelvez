73376b19469689a213303fa3b892488a9fde5350
AbiathGelvez <abvargasg@unal.edu.co>
2025-06-29 23:52:35 -0500

    Update pregunta_07.py
    resultado

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -7,21 +7,20 @@ utilizar pandas, numpy o scipy.
 
 
 def pregunta_07():
-    """
-    Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla
-    contiene un valor posible de la columna 2 y una lista con todas las letras
-    asociadas (columna 1) a dicho valor de la columna 2.
+    agrupacion = {}
 
-    Rta/
-    [(0, ['C']),
-     (1, ['E', 'B', 'E']),
-     (2, ['A', 'E']),
-     (3, ['A', 'B', 'D', 'E', 'E', 'D']),
-     (4, ['E', 'B']),
-     (5, ['B', 'C', 'D', 'D', 'E', 'E', 'E']),
-     (6, ['C', 'E', 'A', 'B']),
-     (7, ['A', 'C', 'E', 'D']),
-     (8, ['E', 'D', 'E', 'A', 'B']),
-     (9, ['A', 'B', 'E', 'A', 'A', 'C'])]
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            partes = linea.strip().split("\t")
+            letra = partes[0]
+            numero = int(partes[1])
+            if numero in agrupacion:
+                agrupacion[numero].append(letra)
+            else:
+                agrupacion[numero] = [letra]
+
+    resultado = sorted(agrupacion.items())
+    return resultado
+
 
     """
