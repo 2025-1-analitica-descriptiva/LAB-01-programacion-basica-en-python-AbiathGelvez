e84d75ea1aa3331c7519935ad43f92e599a932fb
AbiathGelvez <abvargasg@unal.edu.co>
2025-06-30 00:12:17 -0500

    Update pregunta_06.py
    .

--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -7,22 +7,24 @@ utilizar pandas, numpy o scipy.
 
 
 def pregunta_06():
-    """
-    La columna 5 codifica un diccionario donde cada cadena de tres letras
-    corresponde a una clave y el valor despues del caracter `:` corresponde al
-    valor asociado a la clave. Por cada clave, obtenga el valor asociado mas
-    pequeño y el valor asociado mas grande computados sobre todo el archivo.
+    valores_por_clave = {}
 
-    Rta/
-    [('aaa', 1, 9),
-     ('bbb', 1, 9),
-     ('ccc', 1, 10),
-     ('ddd', 0, 9),
-     ('eee', 1, 7),
-     ('fff', 0, 9),
-     ('ggg', 3, 10),
-     ('hhh', 0, 9),
-     ('iii', 0, 9),
-     ('jjj', 5, 17)]
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            partes = linea.strip().split("\t")
+            columna5 = partes[4].split(",")
+            for par in columna5:
+                clave, valor = par.split(":")
+                valor = int(valor)
+                if clave in valores_por_clave:
+                    valores_por_clave[clave].append(valor)
+                else:
+                    valores_por_clave[clave] = [valor]
 
-    """
+    resultado = []
+    for clave in sorted(valores_por_clave.keys()):
+        minimo = min(valores_por_clave[clave])
+        maximo = max(valores_por_clave[clave])
+        resultado.append((clave, minimo, maximo))
+
+    return resultado
