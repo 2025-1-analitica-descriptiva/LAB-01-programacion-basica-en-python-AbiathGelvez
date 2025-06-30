59f7ce1ecf42e205af0151378c43966e43443ace
AbiathGelvez <abvargasg@unal.edu.co>
2025-06-30 00:16:16 -0500

    Update pregunta_12.py
    .

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -5,13 +5,19 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_12():
-    """
-    Genere un diccionario que contengan como clave la columna 1 y como valor
-    la suma de los valores de la columna 5 sobre todo el archivo.
+    suma_por_letra = {}
 
-    Rta/
-    {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            partes = linea.strip().split("\t")
+            letra = partes[0]
+            pares = partes[4].split(",")
+            suma = sum(int(par.split(":")[1]) for par in pares)
 
-    """
+            if letra in suma_por_letra:
+                suma_por_letra[letra] += suma
+            else:
+                suma_por_letra[letra] = suma
+
+    return dict(sorted(suma_por_letra.items()))
