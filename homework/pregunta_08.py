9a6cb4c52ea240e69a2d04148843ddbf27daafa8
AbiathGelvez <abvargasg@unal.edu.co>
2025-06-30 00:14:00 -0500

    Update pregunta_08.py
    .

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -7,23 +7,21 @@ utilizar pandas, numpy o scipy.
 
 
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla
-    contiene  el valor de la segunda columna; la segunda parte de la tupla
-    es una lista con las letras (ordenadas y sin repetir letra) de la
-    primera  columna que aparecen asociadas a dicho valor de la segunda
-    columna.
+    agrupacion = {}
 
-    Rta/
-    [(0, ['C']),
-     (1, ['B', 'E']),
-     (2, ['A', 'E']),
-     (3, ['A', 'B', 'D', 'E']),
-     (4, ['B', 'E']),
-     (5, ['B', 'C', 'D', 'E']),
-     (6, ['A', 'B', 'C', 'E']),
-     (7, ['A', 'C', 'D', 'E']),
-     (8, ['A', 'B', 'D', 'E']),
-     (9, ['A', 'B', 'C', 'E'])]
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            partes = linea.strip().split("\t")
+            letra = partes[0]
+            numero = int(partes[1])
 
-    """
+            if numero not in agrupacion:
+                agrupacion[numero] = set()
+            agrupacion[numero].add(letra)
+
+    resultado = []
+    for numero in sorted(agrupacion.keys()):
+        letras_ordenadas = sorted(agrupacion[numero])
+        resultado.append((numero, letras_ordenadas))
+
+    return resultado
