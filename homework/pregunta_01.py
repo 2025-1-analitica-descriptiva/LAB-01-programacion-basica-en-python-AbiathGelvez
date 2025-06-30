6f1128ccdd8b8dca574f342365fb6354aa20ccff
AbiathGelvez <abvargasg@unal.edu.co>
2025-06-29 20:12:44 -0500

    Update pregunta_01.py
    respuesta

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,12 +5,10 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
-
-    Rta/
-    214
-
-    """
+    suma = 0
+    with open("data.csv", "r") as archivo:
+        for linea in archivo:
+            partes = linea.strip().split("\t")
+            suma += int(partes[1])
+    return suma
